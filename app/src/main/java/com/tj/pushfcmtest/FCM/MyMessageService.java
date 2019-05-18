667b3cd35205de4da2ffe45f31f0cd5f46f0de6e
Chae <chaeakf12@gmail.com>
2019-05-18 13:48:38 +0900

    기본 푸시 알림 받기 테스트

--- app/src/main/java/com/tj/pushfcmtest/FCM/MyMessageService.java
+++ app/src/main/java/com/tj/pushfcmtest/FCM/MyMessageService.java
@@ -1,6 +1,15 @@
 package com.tj.pushfcmtest.FCM;
 
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MyMessageService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+
+        Log.d("새토큰 발급",s);
+    }
 }
